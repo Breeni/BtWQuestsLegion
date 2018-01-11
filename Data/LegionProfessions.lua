3e4915689241c9a50fe1985eed7c85efba6ab091
Breeni <breeni@mythision.com>
2018-01-11 13:41:25 +0000

    Blacksmithing quest chain

--- Data/LegionProfessions.lua
+++ Data/LegionProfessions.lua
@@ -5,16 +5,12 @@ BtWQuests_Chains[BTWQUESTS_CHAIN_LEGION_PROFESSIONS_UNDERLIGHT_ANGLER] = {
     prerequisites = {
         {
             type = "level",
-            level = 110,
-        },
-        {
-            type = "achievement",
-            id = 10596,
+            level = 98,
         },
     },
     completed = {
         type = "quest",
-        id = 41010,
+        id = 38533,
     },
     range = {98, 110},
     items = {
@@ -63,11 +59,346 @@ BtWQuests_Chains[BTWQUESTS_CHAIN_LEGION_PROFESSIONS_UNDERLIGHT_ANGLER] = {
     },
 }
 
+BtWQuests_Chains[BTWQUESTS_CHAIN_LEGION_PROFESSIONS_BLACKSMITHING] = {
+    name = "Blacksmithing",
+    category = BTWQUESTS_CATEGORY_LEGION_PROFESSIONS,
+    expansion = BTWQUESTS_EXPANSION_LEGION,
+    prerequisites = {
+        {
+            type = "level",
+            level = 98,
+        },
+    },
+    completed = {
+        type = "quest",
+        id = 41010,
+    },
+    range = {98, 110},
+    items = {
+        {
+            type = "quest",
+            id = 38499,
+            x = 2,
+            y = 0,
+            connections = {
+                2,
+            }
+        },
+        {
+            type = "quest",
+            id = 48053,
+            x = 4,
+            y = 0,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 39681,
+            x = 3,
+            y = 1,
+            connections = {
+                1, 2,
+            }
+        },
+        {
+            type = "quest",
+            id = 38502,
+            x = 2,
+            y = 2,
+            connections = {
+                2,
+            }
+        },
+        {
+            type = "quest",
+            id = 38501,
+            x = 4,
+            y = 2,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38505,
+            x = 3,
+            y = 3,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38506,
+            x = 3,
+            y = 4,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38507,
+            x = 3,
+            y = 5,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38515,
+            x = 3,
+            y = 6,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38500,
+            x = 3,
+            y = 7,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38563,
+            x = 3,
+            y = 8,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38513,
+            x = 3,
+            y = 9,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38514,
+            x = 3,
+            y = 10,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 39699,
+            x = 3,
+            y = 11,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38519,
+            x = 3,
+            y = 12,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38518,
+            x = 3,
+            y = 13,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38522,
+            x = 3,
+            y = 14,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38523,
+            x = 3,
+            y = 15,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 39702,
+            x = 3,
+            y = 16,
+            connections = {
+                1, 2,
+            }
+        },
+        {
+            type = "quest",
+            id = 39680,
+            x = 2,
+            y = 17,
+            connections = {
+                2,
+            }
+        },
+        {
+            type = "quest",
+            id = 39726,
+            x = 4,
+            y = 17,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 39729,
+            x = 3,
+            y = 18,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38564,
+            x = 3,
+            y = 19,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 44449,
+            x = 3,
+            y = 20,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38524,
+            x = 3,
+            y = 21,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38525,
+            x = 3,
+            y = 22,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38526,
+            x = 3,
+            y = 23,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38527,
+            x = 3,
+            y = 24,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38528,
+            x = 3,
+            y = 25,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38530,
+            x = 3,
+            y = 26,
+            connections = {
+                1, 2
+            }
+        },
+        {
+            type = "quest",
+            id = 38531,
+            x = 2,
+            y = 27,
+            connections = {
+                2,
+            }
+        },
+        {
+            type = "quest",
+            id = 38532,
+            x = 4,
+            y = 27,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38559,
+            x = 3,
+            y = 28,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38833,
+            x = 3,
+            y = 29,
+            connections = {
+                1,
+            }
+        },
+        {
+            type = "quest",
+            id = 38533,
+            x = 3,
+            y = 30,
+        },
+    },
+}
+
 
 BtWQuests_Categories[BTWQUESTS_CATEGORY_LEGION_PROFESSIONS] = {
     name = BTWQUESTS_PROFESSIONS,
     expansion = BTWQUESTS_EXPANSION_LEGION,
     items = {
+        {
+            type = "chain",
+            id = BTWQUESTS_CHAIN_LEGION_PROFESSIONS_BLACKSMITHING,
+        },
         {
             type = "chain",
             id = BTWQUESTS_CHAIN_LEGION_PROFESSIONS_UNDERLIGHT_ANGLER,
