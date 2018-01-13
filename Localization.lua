cae42902ff426bdfecaf8d3c279d66ccfb67cb96
Breeni <breeni@mythision.com>
2018-01-13 10:03:49 +0000

    Inscription

--- Localization.lua
+++ Localization.lua
@@ -78,6 +78,7 @@ BTWQUESTS_PET_SUNDARTER = "Pet: Sun Darter Hatchling"
 BTWQUESTS_MOUNT_LUCID_NIGHTMARE = "Mount: Lucid Nightmare"
 
 BTWQUESTS_PROFESSIONS = "Professions"
+BTWQUESTS_INSCRIPTION = "Inscription"
 BTWQUESTS_FISHING = "Fishing"
 BTWQUESTS_WAIT_NAT_PAGLE = "Wait for Nat Pagle (May require relogging)"
 BTWQUESTS_THE_UNDERLIGHT_ANGLER = "The Underlight Angler"
