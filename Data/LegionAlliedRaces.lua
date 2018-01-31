b6b1060111fd2b06bd9cd7f87b2628dabff320d5
Breeni <breeni@mythision.com>
2018-01-31 17:24:11 +0000

    Added Void Elf quest line

--- Data/LegionAlliedRaces.lua
+++ Data/LegionAlliedRaces.lua
@@ -49,6 +49,48 @@ BtWQuests_Chains[BTWQUESTS_CHAIN_LEGION_ALLIED_RACES_LIGHTFORGED_DRAENEI] = {
         },
     },
 }
+BtWQuests_Chains[BTWQUESTS_CHAIN_LEGION_ALLIED_RACES_VOID_ELF] = {
+    name = "Void Elf",
+    category = BTWQUESTS_CATEGORY_LEGION_ALLIED_RACES,
+    expansion = BTWQUESTS_EXPANSION_LEGION,
+    prerequisites = {
+        {
+            type = "expansion",
+            expansion = 7,
+        },
+        {
+            type = "chain",
+            id = BTWQUESTS_CHAIN_LEGION_ARGUS_WAR_OF_LIGHT_AND_SHADOW,
+        },
+        {
+            type = "reputation",
+            id = 2170,
+            standing = 8,
+        },
+    },
+    completed = {
+        type = "quest",
+        id = 50071,
+    },
+    range = {110},
+    items = {
+        {
+            type = "quest",
+            id = 49787,
+            x = 3,
+            y = 0,
+            connections = {
+                1
+            },
+        },
+        {
+            type = "quest",
+            id = 48962,
+            x = 3,
+            y = 1,
+        },
+    },
+}
 
 BtWQuests_Categories[BTWQUESTS_CATEGORY_LEGION_ALLIED_RACES] = {
     name = "Allied Races",
@@ -58,6 +100,10 @@ BtWQuests_Categories[BTWQUESTS_CATEGORY_LEGION_ALLIED_RACES] = {
         {
             type = "chain",
             id = BTWQUESTS_CHAIN_LEGION_ALLIED_RACES_LIGHTFORGED_DRAENEI,
-        }
+        },
+        {
+            type = "chain",
+            id = BTWQUESTS_CHAIN_LEGION_ALLIED_RACES_VOID_ELF,
+        },
     },
 }
