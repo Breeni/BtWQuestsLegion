dd736ad7bce165096ce07f9d2035db1229802049
Breeni <breeni@mythision.com>
2018-01-31 16:59:03 +0000

    Basics for allied races

---
+++ Data/LegionAlliedRaces.lua
@@ -0,0 +1,7 @@
+BtWQuests_Categories[BTWQUESTS_CATEGORY_LEGION_ALLIED_RACES] = {
+    name = "Allied Races",
+    expansion = BTWQUESTS_EXPANSION_LEGION,
+    buttonImage = 1718211,
+    items = {
+    },
+}
