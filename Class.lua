5d1986a95be975ef8bf18ede179ae170ec9b9889
Breeni <breeni@mythision.com>
2018-07-26 17:55:31 +0100

    Added Here information for Order Halls

--- Class.lua
+++ Class.lua
@@ -325,3 +325,72 @@ BtWQuestsDatabase:AddExpansionItem(BTWQUESTS_EXPANSION_LEGION, {
     type = "category",
     id = BTWQUESTS_CATEGORY_LEGION_CLASS,
 })
+
+-- Death Knight
+BtWQuestsDatabase:AddMapRecursive(647, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+BtWQuestsDatabase:AddMapRecursive(648, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Demon Hunter Zone
+BtWQuestsDatabase:AddMapRecursive(719, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Druid Dreamgrove
+BtWQuestsDatabase:AddMapRecursive(747, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Hunter Lodge
+BtWQuestsDatabase:AddMapRecursive(739, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Mage
+BtWQuestsDatabase:AddMapRecursive(734, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+BtWQuestsDatabase:AddMapRecursive(735, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Monk
+BtWQuestsDatabase:AddMapRecursive(709, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Paladin
+BtWQuestsDatabase:AddMapRecursive(24, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Priest
+BtWQuestsDatabase:AddMapRecursive(702, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Rogue
+BtWQuestsDatabase:AddMapRecursive(626, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Shaman
+BtWQuestsDatabase:AddMapRecursive(726, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Warlock
+BtWQuestsDatabase:AddMapRecursive(717, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
+-- Warrior
+BtWQuestsDatabase:AddMapRecursive(695, {
+    type = "chain",
+    id = BTWQUESTS_CHAIN_LEGION_CLASS_CAMPAIGN,
+})
