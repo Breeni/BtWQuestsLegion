9123bde6d687a391179a2a0149f6d6dc193d5b50
Breeni <breeni@mythision.com>
2018-12-23 23:33:00 +0000

    Added localization file

---
+++ Localization.lua
@@ -0,0 +1,73 @@
+BTWQUESTS_ARTIFACT = "Artifact"
+BTWQUESTS_ORDERHALL = "Order Hall"
+
+BTWQUESTS_RETURN_TO_ORDER_HALL = "Return to your Order Hall"
+BTWQUESTS_GO_TO_THE_FELBLOOD_ALTER = "Go to the Felblood Alter"
+BTWQUESTS_KILL_NIGHTBORNE = "Kill Nightborne"
+BTWQUESTS_GO_TO_LYREA_WINDFEATHER = "Go to Lyrea Windfeather"
+BTWQUESTS_GO_TO_IVANEL_MEADOWSONG = "Go to Ivanel Meadowsong"
+BTWQUESTS_GO_TO_THAEDRIS_FEATHERSONG = "Go to Thaedris Feathersong"
+BTWQUESTS_THAEDRIS_FEATHERSONG = "Thaedris Feathersong"
+BTWQUESTS_KILL_THE_STARVING_ETTIN = "Kill the Starving Ettin"
+BTWQUESTS_RETURN_TO_IRONGROVE_RETRAT = "Return to Irongrove Retreat"
+BTWQUESTS_GO_TO_NIGHTEYES = "Go to Nighteyes"
+BTWQUESTS_NIGHTEYES = "Nighteyes"
+
+BTWQUESTS_COMPLETE_ORDER_HALL_CAMPAIGN = "Complete your Order Campaign"
+BTWQUESTS_THE_THIEVING_APPRENTICE = "The Thieving Apprentice"
+BTWQUESTS_FATE_OF_THE_TIDESKORN = "Fate of the Tideskorn"
+BTWQUESTS_AID_OF_THE_ILLIDARI = "Aid of the Illidari"
+BTWQUESTS_THE_BRADENSBROOK_INVESTIGATION = "The Bradensbrook Investigation"
+BTWQUESTS_RUMBLINGS_NEAR_FELTOTEM = "Rumblings Near Feltotem"
+BTWQUESTS_THE_FOLLY_OF_LEVIA_LAURENCE = "The Folly of Levia Laurence"
+BTWQUESTS_THE_TWISTED_TWINS = "The Twisted Twin"
+
+BTWQUESTS_LIGHTS_HEART = "Light's Heart"
+
+BTWQUESTS_KILL_CHAMPIONS = "Kill Champions"
+BTWQUESTS_KILL_DRAGONS = "Kill Dragons"
+
+BTWQUESTS_DEATHKNIGHT_CAMPAIGN = "Deathlord Campaign"
+BTWQUESTS_DEATHKNIGHT_MOUNT = "Mount: Deathlord's Vilebrood Vanquisher"
+
+BTWQUESTS_DEMONHUNTER_CAMPAIGN = "Slayer Campaign"
+BTWQUESTS_DEMONHUNTER_MOUNT = "Mount: Slayer's Felbroken Shrieker"
+
+BTWQUESTS_DRUID_CAMPAIGN = "Archdruid Campaign"
+BTWQUESTS_DRUID_MOUNT = "Mount: Archdruid's Lunarwing Form"
+
+BTWQUESTS_HUNTER_CAMPAIGN = "Huntmaster Campaign"
+BTWQUESTS_HUNTER_MOUNT = "Mount: Huntmaster's Loyal Wolfhawk"
+
+BTWQUESTS_MAGE_CAMPAIGN = "Archmage Campaign"
+BTWQUESTS_MAGE_MOUNT = "Mount: Archmage's Prismatic Disc"
+
+BTWQUESTS_MONK_CAMPAIGN = "Brewmaster Campaign"
+BTWQUESTS_MONK_MOUNT = "Mount: Ban-Lu, Grandmaster's Companion"
+
+BTWQUESTS_PALADIN_CAMPAIGN = "Highlord Campaign"
+BTWQUESTS_PALADIN_MOUNT = "Mount: Highlord's Golden Charger"
+
+BTWQUESTS_PRIEST_CAMPAIGN = "High Priest Campaign"
+BTWQUESTS_PRIEST_MOUNT = "Mount: High Priest's Lightsworn Seeker"
+
+BTWQUESTS_ROGUE_CAMPAIGN = "Shadowblade Campaign"
+BTWQUESTS_ROGUE_MOUNT = "Mount: Shadowblade's Murderous Omen"
+
+BTWQUESTS_SHAMAN_CAMPAIGN = "Farseer Campaign"
+BTWQUESTS_SHAMAN_MOUNT = "Mount: Farseer's Raging Tempest"
+
+BTWQUESTS_WARLOCK_CAMPAIGN = "Netherlord Campaign"
+BTWQUESTS_WARLOCK_MOUNT = "Mount: Netherlord's Chaotic Wrathsteed"
+
+BTWQUESTS_WARRIOR_CAMPAIGN = "Battelord Campaign"
+BTWQUESTS_WARRIOR_MOUNT = "Mount: Battlelord's Bloodthirsty War Wyrm"
+
+BTWQUESTS_RARE_KOSUMOTH = "Rare: Kosumoth the Hungering"
+BTWQUESTS_MOUNT_RIDDLERS = "Mount: Riddler's Mind-Worm"
+BTWQUESTS_PET_SUNDARTER = "Pet: Sun Darter Hatchling"
+BTWQUESTS_MOUNT_LUCID_NIGHTMARE = "Mount: Lucid Nightmare"
+
+BTWQUESTS_WAIT_NAT_PAGLE = "Wait for Nat Pagle (May require relogging)"
+BTWQUESTS_THE_UNDERLIGHT_ANGLER = "The Underlight Angler"
+BTWQUESTS_FISH_LUMINOUS_PEARL = "Fish up the Luminous Pearl from pools"
