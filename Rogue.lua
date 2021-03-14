f2a6bdaa2768b743e5adff973f4c904b4f36d5bf
Breeni <breeni@mythision.com>
2021-03-14 19:02:59 +0000

    Multiple updates for the Rogue questlines

--- Rogue.lua
+++ Rogue.lua
@@ -34,17 +34,15 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_ASSASSINATION, {
         {
             type = "quest",
             id = 42501,
-            x = 3,
+            x = 2,
             y = 0,
             connections = {
-                1
+                2
             },
         },
         {
             type = "quest",
             id = 42502,
-            x = 3,
-            y = 1,
             connections = {
                 1
             },
@@ -213,6 +211,7 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
     name = L["BTWQUESTS_ROGUE_CAMPAIGN"],
     category = BTWQUESTS_CATEGORY_LEGION_CLASSES_ROGUE,
     expansion = BTWQUESTS_EXPANSION_LEGION,
+    range = {10,45},
     alternatives = {
         BTWQUESTS_CHAIN_LEGION_CLASSES_DEATHKNIGHT_CAMPAIGN,
         BTWQUESTS_CHAIN_LEGION_CLASSES_DEMONHUNTER_CAMPAIGN,
@@ -232,11 +231,21 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             id = BTWQUESTS_CLASS_ID_ROGUE,
         },
     },
+    prerequisites = {
+        {
+            type = "level",
+            level = 10,
+        },
+    },
+    active = {
+        type = "quest",
+        id = 40832,
+        status = {'active', 'completed'}
+    },
     completed = {
         type = "quest",
         id = 37689,
     },
-    range = {98,45},
     items = {
         {
             type = "quest",
@@ -251,7 +260,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 40839,
             x = 3,
-            y = 1,
             connections = {
                 1
             },
@@ -260,7 +268,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 40840,
             x = 3,
-            y = 2,
             connections = {
                 1, 2, 3, 4,
             },
@@ -277,7 +284,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             },
             visible = BtWQuests.LegionArtifactNonSelected("ROGUE"),
             x = 1,
-            y = 3,
             connections = {
                 4
             },
@@ -290,8 +296,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
                 id = 40840,
             },
             visible = BtWQuests.LegionArtifactNonSelected("ROGUE"),
-            x = 3,
-            y = 3,
             connections = {
                 3
             },
@@ -304,8 +308,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
                 id = 40840,
             },
             visible = BtWQuests.LegionArtifactNonSelected("ROGUE"),
-            x = 5,
-            y = 3,
             connections = {
                 2
             },
@@ -356,7 +358,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 40950,
             x = 3,
-            y = 4,
             connections = {
                 1
             },
@@ -383,7 +384,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 40996,
             x = 3,
-            y = 7,
             connections = {
                 1
             },
@@ -392,67 +392,27 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 40997,
             x = 3,
-            y = 8,
-            connections = {
-                --1, 2, 3, 4, 6
-                2
-            },
-        },
-
-
-
-        -- {
-            -- type = "chain",
-            -- id = BTWQUESTS_CHAIN_LEGION_AZSUNA_BEHINDENEMYLINES,
-            -- name = BtWQuests_GetMapName(1015),
-            -- aside = true,
-            -- x = 1,
-            -- y = 7.5,
-        -- },
-        -- {
-            -- type = "chain",
-            -- id = BTWQUESTS_CHAIN_LEGION_VALSHARAH_INTRODUCTION,
-            -- name = BtWQuests_GetMapName(1018),
-            -- aside = true,
-            -- x = 1,
-            -- y = 8.5
-        -- },
-        -- {
-            -- type = "chain",
-            -- id = BTWQUESTS_CHAIN_LEGION_HIGHMOUNTAIN_RIVERMANE_TRIBE,
-            -- name = BtWQuests_GetMapName(1024),
-            -- aside = true,
-            -- x = 5,
-            -- y = 7.5,
-        -- },
-        -- {
-            -- type = "chain",
-            -- id = BTWQUESTS_CHAIN_LEGION_STORMHEIM_GREYMANES_GAMBIT,
-            -- name = BtWQuests_GetMapName(1017),
-            -- aside = true,
-            -- x = 5,
-            -- y = 8.5,
-        -- },
-
-
-
-
-
-        {
-            type = "level",
-            level = 101,
-            x = 5,
-            y = 9.5,
             connections = {
                 1
             },
         },
         {
-            type = "quest",
-            id = 43007,
-            breadcrumb = true,
+            variations = {
+                {
+                    type = "quest",
+                    id = 43007,
+                    restrictions = {
+                        type = "quest",
+                        id = 43007,
+                        status = {'active', 'completed'}
+                    },
+                },
+                {
+                    type = "npc",
+                    id = 101513,
+                },
+            },
             x = 3,
-            y = 10,
             connections = {
                 1
             },
@@ -461,7 +421,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 42139,
             x = 3,
-            y = 11,
             connections = {
                 1
             },
@@ -470,7 +429,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 42140,
             x = 3,
-            y = 12,
             connections = {
                 1
             },
@@ -479,7 +437,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 43013,
             x = 3,
-            y = 13,
             connections = {
                 1
             },
@@ -488,7 +445,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 43014,
             x = 3,
-            y = 14,
             connections = {
                 1
             },
@@ -497,7 +453,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 43015,
             x = 3,
-            y = 15,
             connections = {
                 1
             },
@@ -506,7 +461,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 43958,
             x = 3,
-            y = 16,
             connections = {
                 1
             },
@@ -515,18 +469,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 43829,
             x = 3,
-            y = 17,
-            connections = {
-                2
-            },
-        },
-
-
-        {
-            type = "level",
-            level = 103,
-            x = 5,
-            y = 17.5,
             connections = {
                 1
             },
@@ -535,7 +477,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 44041,
             x = 3,
-            y = 18,
             connections = {
                 1
             },
@@ -544,7 +485,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 44116,
             x = 3,
-            y = 19,
             connections = {
                 1, 2
             },
@@ -553,7 +493,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 44155,
             x = 2,
-            y = 20,
             connections = {
                 2
             },
@@ -561,8 +500,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
         {
             type = "quest",
             id = 44117,
-            x = 4,
-            y = 20,
             connections = {
                 1
             },
@@ -571,40 +508,37 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 44177,
             x = 3,
-            y = 21,
             connections = {
                 1, 2
             },
         },
         {
             type = "quest",
-            id = 44183,
-            x = 5,
-            y = 22,
+            id = 43841,
+            x = 3,
+            connections = {
+                2
+            },
         },
         {
             type = "quest",
-            id = 43841,
-            x = 3,
-            y = 22,
-            connections = {
-                1
-            },
+            id = 44183,
         },
         {
             type = "quest",
             id = 43852,
             x = 3,
-            y = 23,
             connections = {
-                1, 2, 5, 6
+                1,
             },
         },
         {
             type = "quest",
             id = 44181,
-            x = 1,
-            y = 23,
+            x = 3,
+            connections = {
+                1, 2, 3
+            },
         },
 
 
@@ -612,33 +546,6 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
             type = "quest",
             id = 42730,
             x = 1,
-            y = 24,
-            connections = {
-                1
-            },
-        },
-        {
-            type = "quest",
-            id = 44178,
-            x = 1,
-            y = 25,
-            connections = {
-                1, 5
-            },
-        },
-        {
-            type = "quest",
-            id = 44180,
-            x = 0,
-            y = 26,
-        },
-
-
-        {
-            type = "quest",
-            id = 43468,
-            x = 5,
-            y = 24,
             connections = {
                 3
             },
@@ -646,12 +553,35 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_CAMPAIGN, {
         {
             type = "quest",
             id = 42684,
-            x = 3,
-            y = 24,
             connections = {
-                2
+                5
             },
         },
+        {
+            type = "quest",
+            id = 43468,
+            connections = {
+                4
+            },
+        },
+
+
+
+        {
+            type = "quest",
+            id = 44178,
+            x = 1,
+            connections = {
+                1, 3
+            },
+        },
+        {
+            type = "quest",
+            id = 44180,
+            x = 0,
+        },
+
+
 
 
 
@@ -862,6 +792,7 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_ALLIANC
 	},
     category = BTWQUESTS_CATEGORY_LEGION_CLASSES_ROGUE,
     expansion = BTWQUESTS_EXPANSION_LEGION,
+    range = {45},
     alternatives = {
         BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_HORDE,
         BTWQUESTS_CHAIN_LEGION_CLASSES_DEATHKNIGHT_FOLLOWER,
@@ -887,31 +818,35 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_ALLIANC
             id = BTWQUESTS_FACTION_ID_ALLIANCE,
         },
     },
+    prerequisites = {
+        {
+            type = "level",
+            level = 45,
+        },
+        {
+            type = "chain",
+            id = BTWQUESTS_CHAIN_LEGION_BROKENSHORE_BREACHING_THE_TOMB,
+            upto = 47137,
+            completed = {
+                type = "quest",
+                id = 47137,
+                status = {'active', 'completed'}
+            }
+        }
+    },
+    active = {
+        type = "quest",
+        ids = {45833, 46322},
+        status = {'active', 'completed'}
+    },
     completed = {
         type = "quest",
         id = 46059,
     },
-    range = {110},
     items = {
         {
-            type = "chain",
-            id = BTWQUESTS_CHAIN_LEGION_BROKENSHORE_BREACHING_THE_TOMB,
-            userdata = {
-                scrollTo = {
-                    type = "chain",
-                    id = BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_ALLIANCE,
-                },
-            },
-            breadcrumb = true,
-            x = 3,
-            y = 0,
-            connections = {
-                1
-            },
-        },
-        {
-            type = "quest",
-            id = 47137,
+            type = "npc",
+            id = 101513,
             x = 3,
             y = 1,
             connections = {
@@ -1227,6 +1162,7 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_HORDE, 
 	},
     category = BTWQUESTS_CATEGORY_LEGION_CLASSES_ROGUE,
     expansion = BTWQUESTS_EXPANSION_LEGION,
+    range = {45},
     alternatives = {
         BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_ALLIANCE,
         BTWQUESTS_CHAIN_LEGION_CLASSES_DEATHKNIGHT_FOLLOWER,
@@ -1252,31 +1188,35 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_HORDE, 
             id = BTWQUESTS_FACTION_ID_HORDE,
         },
     },
+    prerequisites = {
+        {
+            type = "level",
+            level = 45,
+        },
+        {
+            type = "chain",
+            id = BTWQUESTS_CHAIN_LEGION_BROKENSHORE_BREACHING_THE_TOMB,
+            upto = 47137,
+            completed = {
+                type = "quest",
+                id = 47137,
+                status = {'active', 'completed'}
+            }
+        }
+    },
+    active = {
+        type = "quest",
+        ids = {45833, 46322},
+        status = {'active', 'completed'}
+    },
     completed = {
         type = "quest",
         id = 46059,
     },
-    range = {110},
     items = {
         {
-            type = "chain",
-            id = BTWQUESTS_CHAIN_LEGION_BROKENSHORE_BREACHING_THE_TOMB,
-            userdata = {
-                scrollTo = {
-                    type = "chain",
-                    id = BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_FOLLOWER_HORDE,
-                },
-            },
-            breadcrumb = true,
-            x = 3,
-            y = 0,
-            connections = {
-                1
-            },
-        },
-        {
-            type = "quest",
-            id = 47137,
+            type = "npc",
+            id = 101513,
             x = 3,
             y = 1,
             connections = {
@@ -1589,6 +1529,7 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_MOUNT, {
     name = L["BTWQUESTS_ROGUE_MOUNT"],
     category = BTWQUESTS_CATEGORY_LEGION_CLASSES_ROGUE,
     expansion = BTWQUESTS_EXPANSION_LEGION,
+    range = {45},
     alternatives = {
         BTWQUESTS_CHAIN_LEGION_CLASSES_DEATHKNIGHT_MOUNT,
         BTWQUESTS_CHAIN_LEGION_CLASSES_DEMONHUNTER_MOUNT,
@@ -1610,24 +1551,32 @@ BtWQuestsDatabase:AddChain(BTWQUESTS_CHAIN_LEGION_CLASSES_ROGUE_MOUNT, {
     },
     prerequisites = {
         {
+            type = "level",
+            level = 45,
+        },
+        {
             type = "chain",
             id = BTWQUESTS_CHAIN_LEGION_BROKENSHORE_BREACHING_THE_TOMB,
         },
+    },
+    active = {
+        type = "quest",
+        id = 46103,
+        status = {'active', 'completed'}
     },
     completed = {
         type = "quest",
         id = 46089,
     },
-    range = {110},
     buttonImage = "Interface\\AddOns\\BtWQuestsLegion\\UI-Chain-Rogue-Mount",
     items = {
         {
-            type = "chain",
-            id = BTWQUESTS_CHAIN_LEGION_BROKENSHORE_BREACHING_THE_TOMB,
+            type = "npc",
+            id = 101513,
             x = 3,
-            y = 0,
+            y = 1,
             connections = {
-                1
+                1,
             },
         },
         {
